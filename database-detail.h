cf3f475b703f2ca030f6a2ec62ffb1de66bf9549
megha55 <meghasimak@gmail.com>
2013-03-01 00:00:33 +0530

    database details

---
+++ database-detail.h
@@ -0,0 +1,26 @@
+/*
+ * ===================================================================
+ *
+ *       Filename:  database.h
+ *
+ *    Description:  Database details like database name, username,
+ *                  password.
+ *
+ *        Created:  Friday 22 February 2013 12:46:23  IST
+ *       Compiler:  gcc
+ *
+ *         Author:  Mandeep Kaur, meghasimak@gmail.com
+ *        License:  GNU General Public License
+ *      Copyright:  Copyright (c) 2013, Great Developers
+ *
+ * ===================================================================
+ */
+
+/**-------------------------------------------------------------------
+ *  define variables for DB Details
+ *------------------------------------------------------------------*/
+
+#define SERVER "localhost"
+#define USER "root"
+#define PASSWORD "password"
+#define DATABASE "test"
