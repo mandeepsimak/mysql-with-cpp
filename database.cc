7416e3df7e28372ef47f8af82035d54116872e81
megha55 <meghasimak@gmail.com>
2013-03-01 00:00:58 +0530

    MySQL Class declaration and definition

---
+++ database.cc
@@ -0,0 +1,88 @@
+/*
+ * ===================================================================
+ *
+ *       Filename:  mysql.cc
+ *
+ *    Description:  Definition of MySQL Class for accessing database
+ *
+ *        Created:  Friday 22 February 2013 02:07:49  IST
+ *       Compiler:  gcc
+ *
+ *         Author:  Mandeep Kaur, meghasimak@gmail.com
+ *        License:  GNU General Public License
+ *      Copyright:  Copyright (c) 2013, Great Developers
+ *
+ * ===================================================================
+ */
+
+/**-------------------------------------------------------------------
+ *  Include mysql.h file local header file(declaration of class)
+ *------------------------------------------------------------------*/
+
+#include "database.h"
+
+/**
+ *--------------------------------------------------------------------
+ *       Class:  MySQL
+ *      Method:  MySQL :: MySQL()
+ * Description:  Constructor to initialize database conectivity
+ *--------------------------------------------------------------------
+ */
+
+MySQL :: MySQL()
+{
+    connect = mysql_init(NULL);
+    if ( !connect )
+    {
+        cout << "MySQL Initialization Failed";
+        //return 1;
+    }   
+    connect = mysql_real_connect(connect, SERVER, USER, PASSWORD, DATABASE, 0,NULL,0);
+    if ( connect )
+    {
+        //cout << "Connection Succeeded\n";
+    }
+    else
+    {
+        cout << "Connection Failed\n";
+    }
+}
+
+/**
+ *--------------------------------------------------------------------
+ *       Class:  MySQL
+ *      Method:  MySQL :: ShowTables()
+ * Description:  Show tables in database
+ *--------------------------------------------------------------------
+ */
+
+void MySQL :: ShowTables()
+{
+    mysql_query (connect,"show tables");                              
+                                                                          
+    unsigned int i=0;                                                 
+                                                                         
+    res_set=mysql_store_result(connect);                              
+                                                                          
+    unsigned int numrows = mysql_num_rows(res_set);                   
+                                                                          
+    cout << " Tables in " << DATABASE << " database " << endl;        
+                                                                         
+    while (((row=mysql_fetch_row(res_set)) !=NULL))                   
+    {                                                                 
+        cout << row[i] << endl;                                       
+    }
+}
+
+/**
+ *--------------------------------------------------------------------
+ *       Class:  MySQL
+ *      Method:  MySQL :: ~MySQL()
+ * Description:  Destructor of MySQL class
+ *--------------------------------------------------------------------
+ */
+
+MySQL :: ~MySQL()
+{
+    mysql_close (connect);
+}
---
+++ database.h
@@ -0,0 +1,57 @@
+/*
+ * ===================================================================
+ *
+ *       Filename:  mysql.h
+ *
+ *    Description:  Declaring MySQL class for using database.
+ *
+ *        Created:  Friday 22 February 2013 12:48:40  IST
+ *       Compiler:  gcc
+ *
+ *         Author:  Mandeep Kaur, meghasimak@gmail.com
+ *        License:  GNU General Public License
+ *      Copyright:  Copyright (c) 2013, Great Developers
+ *
+ * ===================================================================
+ */
+
+/**-------------------------------------------------------------------
+ *  Include database-detail.h and other files
+ *------------------------------------------------------------------*/
+
+//#include <iostream>
+//#include <mysql.h>
+#include "header.h"
+
+#include "database-detail.h"
+
+/**
+ * ===================================================================
+ *        Class:  MySQL
+ *  Description:  MySQL class for database accessability
+ * ===================================================================
+ */
+
+class MySQL
+{
+    protected:
+        /** MySQL connectivity Variables */
+        MYSQL *connect;
+        MYSQL_RES *res_set;
+        MYSQL_ROW row;
+        
+        /** Table names */
+
+        int i, j;
+        string query;
+
+    public:
+        /** MySQL Constructor */
+        MySQL();
+
+        /** Function to show tables in database */
+        void ShowTables();
+
+        /** MySQL Destructor */
+        ~MySQL();
+};
