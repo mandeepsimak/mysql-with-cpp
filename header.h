3730d124e364a5112784ace178723bf18e2505d5
megha55 <meghasimak@gmail.com>
2013-03-01 00:01:16 +0530

    header.h

---
+++ header.h
@@ -0,0 +1,30 @@
+/*
+ * ===================================================================
+ *
+ *       Filename:  header.h
+ *
+ *    Description:  Header file for including commom headerfiles.
+ *
+ *        Created:  Friday 15 February 2013 04:53:17  IST
+ *       Compiler:  gcc
+ *
+ *         Author:  Mandeep Kaur, meghasimak@gmail.com
+ *        License:  GNU General Public License
+ *      Copyright:  Copyright (c) 2013, Great Developers
+ *
+ * ===================================================================
+ */
+
+/**-------------------------------------------------------------------
+ *  Header file
+ *------------------------------------------------------------------*/
+
+#include <iostream>
+#include <mysql.h>
+
+/**-------------------------------------------------------------------
+ *  using namespace std
+ *------------------------------------------------------------------*/
+
+using namespace std;
+
